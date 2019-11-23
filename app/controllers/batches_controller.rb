618ee07e190e315b85c174c0debbdc233a3fdb9f
Straga Severa <ought@yandex.ru>
2019-11-23 03:05:24 +0300

    Add file uploading

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,7 +1,18 @@
 # frozen_string_literal: true
 
 class BatchesController < ApplicationController
-  def create; end
+  def create
+    parser = XmlParser.new(batch_params[:file].read)
+    if Batch.exists?(guid: parser.guid)
+      flash[:notice] = 'XML already added'
+    else
+      parser.parse.save
+    end
+  rescue ArgumentError
+    flash[:alert] = 'Malformed XML'
+  ensure
+    redirect_to action: :index
+  end
 
   def index
     @batches = Batch.all
@@ -9,7 +20,11 @@ class BatchesController < ApplicationController
 
   def destroy
     batch = Batch.find_by(id: params[:id])
-    batch&.destroy
+    if batch
+      batch.destroy
+    else
+      flash[:alert] = 'Wrong batch id'
+    end
     redirect_to action: :index
   end
 
--- spec/controllers/batches_controller_spec.rb
+++ spec/controllers/batches_controller_spec.rb
@@ -3,8 +3,8 @@
 require 'rails_helper'
 
 RSpec.describe BatchesController, type: :controller do
-  let!(:batch) { create(:batch) }
-  let!(:another_batch) { create(:batch_seq) }
+  let(:batch) { create(:batch) }
+  let(:another_batch) { create(:batch_seq) }
   let(:invoice) { create(:invoice, batch: batch) }
   let(:invoice_datum) { create(:invoice_datum, invoice: invoice) }
 
@@ -19,10 +19,48 @@ RSpec.describe BatchesController, type: :controller do
     end
   end
 
-  xdescribe 'POST #create' do
-    it 'returns http success' do
-      post :create
-      expect(response).to have_http_status(:success)
+  describe 'POST #create' do
+    context 'with valid file' do
+      before do
+        post :create, params: { batch: { file: fixture_file_upload('files/sales_data.xml') } }
+      end
+
+      it 'redirects to #index' do
+        expect(response).to redirect_to action: :index
+      end
+
+      it 'creates a batch' do
+        batch = Batch.where(guid: '16BA036FCC3D4AF7E05378A06D0ADD38')
+        expect(batch.count).to eq 1
+      end
+
+      it 'does not create second batch' do
+        post :create, params: { batch: { file: fixture_file_upload('files/sales_data.xml') } }
+        expect(Batch.count).to eq 1
+      end
+
+      it 'shows notice if second batch' do
+        post :create, params: { batch: { file: fixture_file_upload('files/sales_data.xml') } }
+        expect(controller).to set_flash[:notice]
+      end
+    end
+
+    context 'with invalid file' do
+      before do
+        post :create, params: { batch: { file: fixture_file_upload('files/invalid_data.xml') } }
+      end
+
+      it 'redirects to #index' do
+        expect(response).to redirect_to action: :index
+      end
+
+      it 'does not create a batch' do
+        expect(Batch.count).to eq 0
+      end
+
+      it 'shows error' do
+        expect(controller).to set_flash[:alert]
+      end
     end
   end
 
@@ -35,6 +73,7 @@ RSpec.describe BatchesController, type: :controller do
       end
 
       it 'deletes a batch' do
+        another_batch # Loading the let block
         expect(Batch.count).to eq 1
         expect(Batch.first).to eq another_batch
       end
@@ -48,7 +87,12 @@ RSpec.describe BatchesController, type: :controller do
       end
 
       it 'does not delete a batch' do
+        another_batch
         expect(Batch.count).to eq 2
+      end
+
+      it 'shows error' do
+        expect(controller).to set_flash[:alert]
       end
     end
   end
